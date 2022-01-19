04e27dab3ef39ec13a3165f8d9cbca1026414425
Nourreddine1920 <Nourreddine1920@github.com>
2022-01-19 16:39:27 +0100

    first commit

---
+++ ParkingSpace.py
@@ -0,0 +1,36 @@
+
+# this file is for selecting spaces in car parking 
+
+import cv2
+import pickle   #  it serializes objects so they can be saved to a file, and loaded in a program again later on.
+
+width, height = 107, 48    
+
+try:
+    with open('CarParkPos', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+
+def mouseClick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+
+    with open('CarParkPos', 'wb') as f:
+        pickle.dump(posList, f)  # storage of all positions using pickle 
+
+
+while True:
+    img = cv2.imread('carParkImg.png')
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)  # draw a rectangle for park spaces 
+
+    cv2.imshow("Image", img)
+    cv2.setMouseCallback("Image", mouseClick)
+    cv2.waitKey(1)
